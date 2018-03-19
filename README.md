c7faeb68679629fbb920c0ee129a612b1a16b126
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-03-19 22:52:51 +1100

    docs & clean

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [![crates.io version][1]][2] [![build status][3]][4]
 [![downloads][5]][6] [![docs.rs docs][7]][8]
 
-Abstract interface to implement random-access instances..
+Abstract interface to implement random-access instances.
 
 - [Documentation][8]
 - [Crate][2]
--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,103 @@
-// #![deny(warnings, missing_docs)]
+#![deny(warnings, missing_docs)]
 // #![cfg_attr(test, feature(plugin))]
 // #![cfg_attr(test, plugin(clippy))]
 
-extern crate failure;
+//! Abstract interface to implement `random-access-storage` instances.
+//!
+//! ## Why?
+//! This module forms a shared interface for reading and writing bytes to
+//! different backends. By having a shared interface, it means implementations
+//! can easily be swapped, depending on the environment.
+//!
+//! ## Usage
+//! ```rust,ignore
+//! extern crate random_access_storage;
+//! use random_access_storage::{Error, Sync, SyncMethods};
+//!
+//! struct S;
+//! impl SyncMethods for S {
+//!   fn open(&self) -> Result<(), Error> {
+//!     // ...
+//!   }
+//!   fn write(&self, offset: u64, data: Vec<u8>) -> Result<(), Error> {
+//!     // ...
+//!   }
+//!   fn read(&self, offset: u64, length: u64) -> Result<Vec<u8>, Error> {
+//!     // ...
+//!   }
+//!   fn del(&self, offset: u64, length: u64) -> Result<(), Error> {
+//!     // ...
+//!   }
+//! }
+//!
+//! let file = Sync::new(SyncMethods);
+//! file.write(0, b"hello")?;
+//! file.write(0, b" world")?;
+//! let text = file.read(0, 11,)?;
+//! assert!(text, b"hello world");
+//! ```
+
+pub extern crate failure;
 
 pub use failure::Error;
 
+/// Methods that need to be implemented for the `Sync` struct.
 pub trait SyncMethods {
+  /// Open the backend.
   fn open(&self) -> Result<(), Error>;
+
+  /// Write bytes at an offset to the backend.
   fn write(&self, offset: u64, data: Vec<u8>) -> Result<(), Error>;
+
+  /// Read a sequence of bytes at an offset from the backend.
   fn read(&self, offset: u64, length: u64) -> Result<Vec<u8>, Error>;
+
+  /// Delete a sequence of bytes at an offset from the backend.
   fn del(&self, offset: u64, length: u64) -> Result<(), Error>;
 }
 
+/// A synchronous implementation of `random_access_storage`.
 #[derive(Debug)]
 pub struct Sync<T> {
-  pub handler: T,
-  pub is_open: bool,
+  handler: T,
+  is_open: bool,
 }
 
 impl<T> Sync<T>
 where
   T: SyncMethods,
 {
-  pub fn write(&self, offset: u64, data: Vec<u8>) -> Result<(), Error> {
+  /// Create a new `Sync` instance.
+  pub fn new(handler: T) -> Sync<T> {
+    Sync {
+      handler: handler,
+      is_open: false,
+    }
+  }
+
+  /// Write bytes at an offset. Calls `SyncMethods::write` under the hood.
+  pub fn write(&mut self, offset: u64, data: Vec<u8>) -> Result<(), Error> {
     if !self.is_open {
       T::open(&self.handler)?;
+      self.is_open = true;
     }
     T::write(&self.handler, offset, data)
   }
 
-  pub fn read(&self, offset: u64, length: u64) -> Result<Vec<u8>, Error> {
+  /// Write bytes from an offset. Calls `SyncMethods::read` under the hood.
+  pub fn read(&mut self, offset: u64, length: u64) -> Result<Vec<u8>, Error> {
     if !self.is_open {
       T::open(&self.handler)?;
+      self.is_open = true;
     }
     T::read(&self.handler, offset, length)
   }
 
-  pub fn del(&self, offset: u64, length: u64) -> Result<(), Error> {
+  /// Delete bytes from an offset. Calls `SyncMethods::del` under the hood.
+  pub fn del(&mut self, offset: u64, length: u64) -> Result<(), Error> {
     if !self.is_open {
       T::open(&self.handler)?;
+      self.is_open = true;
     }
     T::del(&self.handler, offset, length)
   }
