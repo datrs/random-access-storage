62fec35055a2f63b08efc3fc7a9a99751db3d0c0
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-03-20 16:36:11 +1100

    impl husk methods

--- src/lib.rs
+++ src/lib.rs
@@ -19,10 +19,10 @@
 //!   fn open(&self) -> Result<(), Error> {
 //!     // ...
 //!   }
-//!   fn write(&self, offset: u64, data: Vec<u8>) -> Result<(), Error> {
+//!   fn write(&self, offset: u64, data: &[u8]) -> Result<(), Error> {
 //!     // ...
 //!   }
-//!   fn read(&self, offset: u64, length: u64) -> Result<Vec<u8>, Error> {
+//!   fn read(&self, offset: u64, length: u64) -> Result<&[u8], Error> {
 //!     // ...
 //!   }
 //!   fn del(&self, offset: u64, length: u64) -> Result<(), Error> {
@@ -44,23 +44,28 @@ pub use failure::Error;
 /// Methods that need to be implemented for the `Sync` struct.
 pub trait SyncMethods {
   /// Open the backend.
-  fn open(&self) -> Result<(), Error>;
+  fn open(&mut self) -> Result<(), Error>;
 
   /// Write bytes at an offset to the backend.
-  fn write(&self, offset: u64, data: Vec<u8>) -> Result<(), Error>;
+  fn write(&mut self, offset: u64, data: &[u8]) -> Result<(), Error>;
 
   /// Read a sequence of bytes at an offset from the backend.
-  fn read(&self, offset: u64, length: u64) -> Result<Vec<u8>, Error>;
+  fn read(&mut self, offset: u64, length: u64) -> Result<&[u8], Error>;
 
   /// Delete a sequence of bytes at an offset from the backend.
-  fn del(&self, offset: u64, length: u64) -> Result<(), Error>;
+  fn del(&mut self, offset: u64, length: u64) -> Result<(), Error>;
+
+  // fn close ();
+  // fn destroy ();
+  // fn stat ();
 }
 
-/// A synchronous implementation of `random_access_storage`.
+/// Create a synchronous instance.
 #[derive(Debug)]
 pub struct Sync<T> {
+  /// Check whether or not the file has been opened.
+  pub opened: bool,
   handler: T,
-  is_open: bool,
 }
 
 impl<T> Sync<T>
@@ -71,34 +76,38 @@ where
   pub fn new(handler: T) -> Sync<T> {
     Sync {
       handler: handler,
-      is_open: false,
+      opened: false,
     }
   }
 
   /// Write bytes at an offset. Calls `SyncMethods::write` under the hood.
-  pub fn write(&mut self, offset: u64, data: Vec<u8>) -> Result<(), Error> {
-    if !self.is_open {
-      T::open(&self.handler)?;
-      self.is_open = true;
+  pub fn write(&mut self, offset: u64, data: &[u8]) -> Result<(), Error> {
+    if !self.opened {
+      T::open(&mut self.handler)?;
+      self.opened = true;
     }
-    T::write(&self.handler, offset, data)
+    T::write(&mut self.handler, offset, data)
   }
 
   /// Write bytes from an offset. Calls `SyncMethods::read` under the hood.
-  pub fn read(&mut self, offset: u64, length: u64) -> Result<Vec<u8>, Error> {
-    if !self.is_open {
-      T::open(&self.handler)?;
-      self.is_open = true;
+  pub fn read(&mut self, offset: u64, length: u64) -> Result<&[u8], Error> {
+    if !self.opened {
+      T::open(&mut self.handler)?;
+      self.opened = true;
     }
-    T::read(&self.handler, offset, length)
+    T::read(&mut self.handler, offset, length)
   }
 
   /// Delete bytes from an offset. Calls `SyncMethods::del` under the hood.
   pub fn del(&mut self, offset: u64, length: u64) -> Result<(), Error> {
-    if !self.is_open {
-      T::open(&self.handler)?;
-      self.is_open = true;
+    if !self.opened {
+      T::open(&mut self.handler)?;
+      self.opened = true;
     }
-    T::del(&self.handler, offset, length)
+    T::del(&mut self.handler, offset, length)
   }
+
+  // pub fn close () {}
+  // pub fn destroy () {}
+  // pub fn stat () {}
 }
